da743d4ad60ad36bb841375b2536b0304d4ce2d9
skatpgusskat <skatpgusskat@naver.com>
2019-05-02 21:47:30 +0900

    Get note information

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ window.addEventListener('keydown', onKeyDown);
 function onKeyDown(keyboardEvent: KeyboardEvent) {
   console.log(keyboardEvent);
 }
+
+async function initializeAudio() {
+  const response = await fetch('note.json');
+  const notes = await response.json();
+  console.log(Object.keys(notes));
+}
+
+initializeAudio();
